fc424441b5046ae7eb68a21c60ebc416d3b01fbe
Josh Bruegger <josh@bruegger.it>
2026-03-21 23:41:35 +0100

    fix: add missing slurm script

---
+++ SLURM/cc_vs_watershed.sh
@@ -0,0 +1,12 @@
+sbatch SLURM/evaluate_models_and_plot.sh \
+  --model-dir /scratch/s4361687/GrainSeg/models \
+  --image-dir /scratch/s4361687/GrainSeg/dataset/train/cropped \
+  --mask-dir /scratch/s4361687/GrainSeg/dataset/train/cropped \
+  --output-dir /scratch/s4361687/GrainSeg/eval/cc_vs_watershed \
+  --watershed-tune-root /scratch/s4361687/GrainSeg/runs/watershed_tune
+
+sbatch SLURM/evaluate_models_and_plot.sh \
+  --model-dir /scratch/s4361687/GrainSeg/models \
+  --image-dir /scratch/s4361687/GrainSeg/dataset/train/cropped \
+  --mask-dir /scratch/s4361687/GrainSeg/dataset/train/cropped \
+  --output-dir /scratch/s4361687/GrainSeg/eval/cc
